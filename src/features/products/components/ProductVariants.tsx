71cb58980cd86afc680977a6b7443f916d4350da
yinhangc <yinhang265@gmail.com>
2024-04-16 16:09:12 +0800

    dynamic form in progress

--- src/features/products/components/ProductVariants.tsx
+++ src/features/products/components/ProductVariants.tsx
@@ -1,29 +1,77 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 
 type Option = {
+  id: string;
   name: string;
-  values: {
-    label: string;
-    price: number;
-  }[];
+  values: OptionValue[];
+};
+type OptionValue = {
+  id: string;
+  label: string;
+  price: number;
 };
 type ProductOptionInputProps = {
   option: Option;
-  setOptions: any;
+  handleAddOptionValue: (optionId: string, optionValue: OptionValue) => void;
 };
 
 const ProductOptionInput: React.FC<ProductOptionInputProps> = (props) => {
-  const { option, setOptions } = props;
+  const { option, handleAddOptionValue } = props;
+  const { register, control, watch } = useForm<Option>({
+    defaultValues: {
+      id: option.id,
+      name: '',
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'values',
+  });
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      console.log('WATCH', value, name, type);
+      if (
+        name === 'name' &&
+        !!value.name &&
+        value.name.length > 0 &&
+        !!value.values &&
+        value.values.length === 0
+      ) {
+        // handleAddOptionValue(option.id, { label: '', price: 0 });
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [handleAddOptionValue, watch]);
+
   return (
     <div className="flex w-full flex-col gap-y-1">
-      <label htmlFor="name">選項名字</label>
-      <input
-        placeholder="Option Name"
-        id="name"
-        className="rounded border border-gray-400 px-4 py-2"
-      />
+      <div className="mb-1 flex flex-col">
+        <label htmlFor="name">選項名字</label>
+        <input
+          placeholder="Option Name"
+          className="rounded border border-gray-400 px-4 py-2"
+          {...register('name')}
+        />
+      </div>
+      <div className="ml-6 flex flex-col">
+        {fields.map((field, index) => {
+          return (
+            <div>
+              <label htmlFor="name">選項數值</label>
+              <input
+                key={field.id}
+                placeholder="Option Value"
+                className="rounded border border-gray-400 px-4 py-2"
+                {...register(`values.${index}` as const)}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
@@ -31,23 +79,34 @@ const ProductOptionInput: React.FC<ProductOptionInputProps> = (props) => {
 export const ProductVariants = () => {
   const [options, setOptions] = useState<Option[]>([]);
 
-  const handleAdd = () => {
-    setOptions([...options, { name: '', values: [] }]);
+  const handleAddOption = () => {
+    setOptions([
+      ...options,
+      { id: Math.random().toString(36).substr(2, 5), name: '', values: [] },
+    ]);
+  };
+
+  const handleAddOptionValue = (optionId: string, optionValue: OptionValue) => {
+    const updatedOption = options.find((opt) => opt.id === optionId);
+    if (!updatedOption) return;
   };
 
   return (
     <div>
       {options.length > 0 && (
-        <div className="mb-6">
+        <div className="mb-6 flex flex-col gap-y-6">
           {options.map((option) => (
-            <ProductOptionInput option={option} setOptions={setOptions} />
+            <ProductOptionInput
+              option={option}
+              handleAddOptionValue={handleAddOptionValue}
+            />
           ))}
         </div>
       )}
       <button
         type="button"
         className="rounded border border-blue-600 px-4 py-1 text-blue-600"
-        onClick={handleAdd}
+        onClick={handleAddOption}
       >
         <FontAwesomeIcon icon={faPlus} className="mr-1" />
         增加選項
--- src/features/products/routes/AddProduct.tsx
+++ src/features/products/routes/AddProduct.tsx
@@ -57,7 +57,7 @@ export const AddProduct = () => {
       <form onSubmit={handleSubmit(onSubmit)} className="w-full">
         <div className="grid grid-cols-12 gap-x-4">
           <div className="col-span-8 flex flex-col gap-y-6">
-            <div className="flex flex-col gap-y-4 rounded-md bg-white p-4 shadow-md">
+            <div className="flex flex-col gap-y-4 rounded-md bg-white px-6 py-4 shadow-md">
               <h3 className="font-medium">一般資料</h3>
               <div className="w-full">
                 <FormInputField
@@ -80,7 +80,7 @@ export const AddProduct = () => {
                 ></Controller>
               </div>
             </div>
-            <div className="flex flex-col gap-y-4 rounded-md bg-white p-4 shadow-md">
+            <div className="flex flex-col gap-y-4 rounded-md bg-white px-6 py-4 shadow-md">
               <h3 className="font-medium">相片</h3>
               <Controller
                 control={control}
@@ -90,13 +90,13 @@ export const AddProduct = () => {
                 )}
               ></Controller>
             </div>
-            <div className="flex flex-col gap-y-4 rounded-md bg-white p-4 shadow-md">
+            <div className="flex flex-col gap-y-4 rounded-md bg-white px-6 py-4 shadow-md">
               <h3 className="font-medium">選項、價格和庫存</h3>
               <ProductVariants />
             </div>
           </div>
           <div className="col-span-4 flex flex-col gap-y-6">
-            <div className="flex flex-col gap-y-4 rounded-md bg-white p-4 shadow-md">
+            <div className="flex flex-col gap-y-4 rounded-md bg-white px-6 py-4 shadow-md">
               <h3 className="font-medium">狀態</h3>
               <FormDropdown
                 name="status"
